67974f55f0d571c7a57f2fb92d49fdb5b7e290c5
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-15 19:03:00 -0300

    Generalize page% into move-file

--- lib/markup.lisp
+++ lib/markup.lisp
@@ -25,14 +25,14 @@
 (defparameter +build-path+
   (merge-pathnames #p"build/"))
 
-(defun page% (pathname content)
+(defun move-file (pathname output-type content)
   (let* ((content-dir (make-pathname
                        :directory (pathname-directory pathname)))
          (build-dir (merge-pathnames content-dir +build-path+))
          (output-path (make-pathname
                        :directory (pathname-directory build-dir)
                        :name (pathname-name pathname)
-                       :type "html")))
+                       :type output-type)))
     (ensure-directories-exist build-dir)
     (with-open-file (stream output-path
                             :direction :output
@@ -52,4 +52,4 @@
         forms))))
 
 (defmacro page (pathname)
-  `(page% ,pathname ,@(read-file pathname)))
+  `(move-file ,pathname "html" ,@(read-file pathname)))
