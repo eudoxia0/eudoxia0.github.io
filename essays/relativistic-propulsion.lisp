256188ab193ed3275be7184e1789c26f4cf68179
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-15 23:49:59 -0300

    Add this file so I will remember to write it eventually

---
+++ essays/relativistic-propulsion.lisp
@@ -0,0 +1,8 @@
+;;;; Discuss Project Valkyrie, lack of specifics and design mistakes,
+;;;; improvements. Magnetic monopoles. Monopole-catalyzed fusion. Monopole
+;;;; production (Size/luminosity of collider) speculation? Implications to
+;;;; speculation re:interstellar travel. Possibly read Krugman's paper on
+;;;; insterstellar trade and contrast it.
+
+(title [Relativistic Propulsion]
+  (:p [This will have content, eventually.]))
