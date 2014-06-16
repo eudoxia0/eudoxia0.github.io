d0835eadbf3674757c6869173ca018ef716c53c4
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-15 23:44:24 -0300

    Change indent, add section to about page

--- about.lisp
+++ about.lisp
@@ -1,3 +1,8 @@
-(post [About]
-  (:h1 [About Me])
-  (:h1 [Colophon]))
+(post 
+ [About]
+ (:h1 [About Me])
+ (:h1 [Colophon])
+ (:h2 [Why not use Github Pages?])
+ (:p [Because they require -- horror of horrors! -- that I pollute my git repo
+     with HTML and CSS. Repositories are for source code and source code only,
+     not build output.]))
