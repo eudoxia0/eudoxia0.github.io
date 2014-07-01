7bb810cb3f7a0ffaadf3108c9272db08df40b33d
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-01 19:27:28 -0300

    Add essay template renderer

--- lib/templates.lisp
+++ lib/templates.lisp
@@ -2,8 +2,8 @@
 
 ;;; Templates
 
-(register-emb "layout" #p"templates/layout.tmpl")
-(register-emb "post" #p"templates/post.tmpl")
+(register-emb "layout" (path #p"templates/layout.tmpl"))
+(register-emb "post" (path #p"templates/post.tmpl"))
 
 (defun render-layout (title content)
   (execute-emb "layout" :env (list :title title
@@ -17,4 +17,11 @@
 
 (with-backend :html
   (defrule page () (a tree)
-    (render-layout (gethash "title" a) (emit tree))))
+    (render-layout (gethash "title" a) (emit tree)))
+  (defrule essay () (a tree)
+    (let ((title (gethash "title" a))
+          (links (pop-by-name tree "links")))
+      ;; Emit the links before the rest
+      (emit links)
+      (print wax.html::*links*)
+      (render-post title nil (emit tree)))))
