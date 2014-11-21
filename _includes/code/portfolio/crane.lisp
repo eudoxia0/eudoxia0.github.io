9248155bdb722ad1d6cd573b809d947476858b14
Fernando Borretti <eudoxiahp@gmail.com>
2014-11-21 19:31:02 -0200

    Minor corrections

--- _includes/code/portfolio/crane.lisp
+++ _includes/code/portfolio/crane.lisp
@@ -1,7 +1,7 @@
 (deftable ship ()
   (name :type text :indexp t)
   (flag :type text :nullp nil)
-  (tonnage :type integer :nullp nil)
+  (tonnage :type double :nullp nil)
   (length :type integer :nullp nil))
 
 (create 'ship :name "Mærsk Mc-Kinney Møller")
