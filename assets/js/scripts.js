c8d916b5fa85c9cb960d827e712d4ce651b81a8b
Fernando Borretti <eudoxiahp@gmail.com>
2015-06-07 16:37:04 -0300

    Some image style, wrap all images in links to themselves

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,13 +6,23 @@ function fixCodeClasses(block) {
     $(block).attr('class', new_class);
 }
 
-function highlightEverything(block) {
+function highlightEverything() {
+  /* Highlight every code block */
+  $('pre code').each(function(i, block) {
+    fixCodeClasses(block);
     hljs.highlightBlock(block);
+  });
+}
+
+function linkImages() {
+  /* Wrap all images in links to their source */
+  $('article img').each( function() {
+    const source = $(this).attr('src');
+    $(this).wrap('<a href="' + source + '"></a>');
+  });
 }
 
 $(document).ready(function() {
-    $('pre code').each(function(i, block) {
-        fixCodeClasses(block);
-        highlightEverything(block);
-    });
+  highlightEverything();
+  linkImages();
 });
