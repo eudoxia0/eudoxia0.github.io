a6bf5f8ebf54115fa44837564f0b634932557ab6
Fernando Borretti <eudoxiahp@gmail.com>
2015-08-20 23:06:05 -0300

    Include some code

---
+++ _includes/code/newsbeuter.lisp
@@ -0,0 +1,34 @@
+(ql:quickload (list :dbi :yason :local-time :group-by))
+
+(defvar *connection*
+  (dbi:connect :sqlite3
+               :database-name (merge-pathnames #p".newsbeuter/cache.db"
+                                               (user-homedir-pathname))))
+
+(let* ((output (merge-pathnames #p"lisp.json"
+                                (user-homedir-pathname)))
+       (query (dbi:prepare *connection*
+                           "SELECT pubDate FROM rss_item WHERE feedurl = ?"))
+       (results (mapcar #'(lambda (result)
+                            (local-time:unix-to-timestamp
+                             (getf result :|pubDate|)))
+                        (dbi:fetch-all
+                         (dbi:execute query "http://planet.lisp.org/github.atom")))))
+  (with-open-file (stream (merge-pathnames #p"lisp.json"
+                                           (user-homedir-pathname))
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+    (yason:encode
+     (mapcar #'(lambda (group)
+                 (list
+                  (local-time:format-timestring nil
+                                                (first group)
+                                                :format (list :year #\- :month #\- :day))
+                  (1- (length group))))
+             (group-by:group-by-repeated results
+                                         :keys (list #'identity)
+                                         :tests (list #'(lambda (a b)
+                                                          (= (local-time:day-of a)
+                                                             (local-time:day-of b))))))
+     stream)))
