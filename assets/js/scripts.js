3a6b51eb4f0c1f5f994811bb61b23aec16cb134d
Fernando Borretti <eudoxiahp@gmail.com>
2017-11-30 00:39:53 -0300

    Fix a bug in the JavaScript for cleaning up code tags

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,18 @@
 function fixCodeClasses(block) {
-    /* Jekyll emits <code> tags that have classes like 'language-lisp' or
-       'language-markdown'. highlight.js allegedly supports those. This isn't
-       the case. So this function removes the 'language-' prefix. */
-    const new_class = $(block).attr('class').replace('language-', '');
+  /* Jekyll emits <code> tags that have classes like 'language-lisp' or
+     'language-markdown'. highlight.js allegedly supports those. This isn't the
+     case. So this function removes the 'language-' prefix. */
+  const old_class = $(block).attr('class');
+  if (old_class) {
+    const new_class = old_class.replace('language-', '');
     $(block).attr('class', new_class);
+  }
 }
 
 function highlightEverything() {
   /* Highlight every code block */
   $('pre code').each(function(i, block) {
+    console.log(block);
     fixCodeClasses(block);
     hljs.highlightBlock(block);
   });
