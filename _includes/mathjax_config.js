51265f479230f30e69dadf3756115dd7d99fa3ff
Fernando Borretti <eudoxiahp@gmail.com>
2017-12-05 22:59:07 -0300

    Remove mathjax

--- _includes/mathjax_config.js
+++
@@ -1,8 +0,0 @@
-MathJax.Hub.Config({
-    "tex2jax": {"inlineMath": [['$','$'], ['\\(','\\)']]}
-});
-MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
-    MathJax.Hub.Insert(MathJax.InputJax.TeX.Definitions.macros,{
-        cancel: ["Extension","cancel"],
-    });
-});
