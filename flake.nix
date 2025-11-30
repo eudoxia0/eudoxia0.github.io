a7f628c4f94a72fd1dd682ec4b96d6fed6cd6135
Fernando Borretti <fernando@borretti.me>
2025-11-30 15:04:18 +1100

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Jekyll development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              jekyll
+            ];
+
+            shellHook = ''
+              export GEM_HOME="$PWD/.gem"
+              export PATH="$GEM_HOME/bin:$PATH"
+            '';
+          };
+        });
+}
